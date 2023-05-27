413fd31b3f6db7da998b7afbe8ec4ef245c15aae
Yijun Jeon <gkqls813@gmail.com>
2023-05-27 19:27:13 +0900

    Implement search draft

--- js/setHash.js
+++ js/setHash.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+  // 기본 폼 제출 동작 제거
+  document
+    .getElementById('search-form')
+    .addEventListener('submit', function (event) {
+      event.preventDefault();
+    });
+
   $('.rank').click(function () {
     event.preventDefault(); // 기본 동작 방지
     // 하위 태그 div의 class 속성 가져오기
@@ -11,5 +18,12 @@ $(document).ready(function () {
     // 하위 태그 div의 class 속성 가져오기
     var title = $(this).find('a').attr('title');
     window.location.href = `product.html#${title}`;
+  });
+
+  $('.input-search-submit').click(function () {
+    event.preventDefault(); // 기본 동작 방지
+    // 하위 태그 div의 class 속성 가져오기
+    var value = document.getElementsByClassName('input-search')[0].value;
+    window.location.href = `product.html#${value}`;
   });
 });
